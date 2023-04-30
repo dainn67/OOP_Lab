a149b16f20bd0aa0f571602a67d407504ada35b1
Nguyễn Đại <112848222+dainn67@users.noreply.github.com>
2023-04-30 10:30:32 +0700

    push AimsProject

---
+++ Lab02/AimsProject/src/main_package/Aim.java
@@ -0,0 +1,26 @@
+package main_package;
+
+public class Aim {
+	public static void main(String[] args) {
+		System.out.print("HI");
+		Cart anOrderCart = new Cart();
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+		anOrderCart.addDigitalVideoDisc(dvd1);
+		
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+		anOrderCart.addDigitalVideoDisc(dvd2);
+		
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("ALadin", "Animation", 18.99f);
+		anOrderCart.addDigitalVideoDisc(dvd3);
+		
+//		System.out.println("Total cost is: ");
+//		System.out.println(anOrderCart.totalCost());
+		
+		System.out.println();
+		
+		DigitalVideoDisc dvdTmp = new DigitalVideoDisc("ALadin");
+		anOrderCart.removeDigitalVideoDisc(dvdTmp);
+		System.out.println();
+		anOrderCart.prtOrder();
+	}
+}
