080332335a6b1fcf45d0736f5202ab7a189354be
Nguyễn Đại <112848222+dainn67@users.noreply.github.com>
2023-05-14 22:55:44 +0700

    OthersProject

---
+++ Lab03/OtherProjects/src/hust/soict/ict/lab01/HeightOfnStars.java
@@ -0,0 +1,19 @@
+package hust.soict.ict.lab01;
+import java.util.Scanner;
+
+public class HeightOfnStars {
+    public static void main(String[] args) {
+    	Scanner scanner = new Scanner(System.in);
+    	
+    	System.out.print("Enter number of rows: ");
+    	int n = scanner.nextInt();
+    	String resString = "";
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) resString += " ";
+            for (int k = 1; k <= 2 * i - 1; k++) resString += "*";
+            resString += "\n";
+        }
+        System.out.print(resString);
+    }
+}
